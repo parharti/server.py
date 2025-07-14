0efe876ac00ed761bf4108356347eeb17a716e57
Prisha Sharma <140067623+parharti@users.noreply.github.com>
2025-07-14 16:21:42 +0530

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 sarvam_client = SarvamAI(api_subscription_key="b5d9635d-8168-411e-9ed8-0c2e33114f5a")
 
 # Option 1: HARDCODED ngrok URL (update this every time ngrok restarts)
-RASA_BASE_URL = "https://2ceb360f014f.ngrok-free.app"
+RASA_BASE_URL = "https://7b41e7807dc0.ngrok-free.app "
 
 # Option 2: Auto-fetch ngrok (uncomment if you want automation)
 # def get_ngrok_url():
